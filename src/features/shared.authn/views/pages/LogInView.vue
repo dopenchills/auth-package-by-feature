2cffadfc2b76b8ce3d5029d87a38b3ce0c921c43
dopenchills <dopenchills@gmail.com>
2025-09-27 17:10:25 +0900

    remove unnecessary files

--- src/features/shared.authn/views/pages/LogInView.vue
+++ src/features/shared.authn/views/pages/LogInView.vue
@@ -1,19 +1,6 @@
 <template>
-  <div class="main">
-    <h1>This is an login page</h1>
-    <button @click="async () => await authService.logIn()">login</button>
-  </div>
+  <button @click="async () => await authService.logIn()">login</button>
 </template>
-
-<style>
-@media (min-width: 1024px) {
-  .main {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-  }
-}
-</style>
 
 <script setup lang="ts">
 import { authService } from '../variables/authService'
